213faddd985c4a4baf141f06488e37b9231a66ef
Fredrik Adelöw <freben@gmail.com>
2022-01-16 20:03:12 +0100

    Introduce @backstage/plugin-auth-node
    Signed-off-by: Fredrik Adelöw <freben@gmail.com>

--- plugins/auth-node/src/identity/IdentityClient.test.ts
+++ plugins/auth-node/src/identity/IdentityClient.test.ts
@@ -14,17 +14,19 @@
  * limitations under the License.
  */
 
-import { JWT, JSONWebKey } from 'jose';
-import { rest } from 'msw';
-import { setupServer } from 'msw/node';
 import {
   getVoidLogger,
   PluginEndpointDiscovery,
 } from '@backstage/backend-common';
+// import {
+//   MemoryKeyStore,
+//   TokenFactory,
+// } from '@backstage/plugin-auth-backend';
+import { JSONWebKey, JWT } from 'jose';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 import { IdentityClient } from './IdentityClient';
-import { MemoryKeyStore } from './MemoryKeyStore';
-import { TokenFactory } from './TokenFactory';
-import { KeyStore } from './types';
+import { KeyStore, TokenIssuer } from './types';
 
 const logger = getVoidLogger();
 
@@ -48,7 +50,7 @@ const discovery: PluginEndpointDiscovery = {
 
 describe('IdentityClient', () => {
   let client: IdentityClient;
-  let factory: TokenFactory;
+  let factory: TokenIssuer;
   let keyStore: KeyStore;
   const keyDurationSeconds = 5;
 
@@ -225,7 +227,7 @@ describe('IdentityClient', () => {
       expect(token).toEqual('token');
     });
 
-    it('should return Bearer token despite unconventionial case', async () => {
+    it('should return Bearer token despite unconventional case', async () => {
       const token = IdentityClient.getBearerToken('bEARER token');
       expect(token).toEqual('token');
     });
--- plugins/auth-node/src/index.ts
+++ plugins/auth-node/src/index.ts
@@ -14,18 +14,18 @@
  * limitations under the License.
  */
 
-import { AuthResponse } from '@backstage/plugin-auth-node';
-
 /**
- * Payload sent as a post message after the auth request is complete.
- * If successful then has a valid payload with Auth information else contains an error.
+ * Shared facilities of `@backstage/plugin-auth-backend`.
+ *
+ * @packageDocumentation
  */
-export type WebMessageResponse =
-  | {
-      type: 'authorization_response';
-      response: AuthResponse<unknown>;
-    }
-  | {
-      type: 'authorization_response';
-      error: Error;
-    };
+
+export * from './identity';
+export type {
+  AuthResponse,
+  BackstageIdentity,
+  BackstageIdentityResponse,
+  BackstageSignInResult,
+  BackstageUserIdentity,
+  ProfileInfo,
+} from './types';
